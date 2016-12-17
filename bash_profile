496adfbb4545d8503f3c8f29384d31eed9e240fb
Sean Sall <sean@arterys.com>
2016-12-17 12:40:48 -0800

    Initial commit

---
+++ bash_profile
@@ -0,0 +1,35 @@
+### Setup Git info. in bash prompt ###
+source ~/.git-prompt.sh
+
+if [[ $(uname) == "Darwin" ]]; then
+    host=local
+else
+    host=$(hostname)
+fi
+
+export GIT_PS1_SHOWUPSTREAM=verbose, git
+export GIT_PS1_SHOWUNTRACKEDFILES=enabled
+export GIT_PS1_SHOWDIRTYSTATE=enabled
+PS1='\[\033[01;32m\]\u@${host}\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;33m\]$(__git_ps1 " (%s)")\[\033[00m\]\$ '
+
+test -f ~/.git-completion.sh && . $_ # Add git-completion
+
+### Add pems via SSH ###
+if [ -d ~/.pems ]; then
+    for pem in $(find ~/.pems -name *.pem); do
+        ssh-add $pem >/dev/null 2>&1
+    done
+fi
+
+### Alter path for custom executables
+export PATH="~/.dotfiles/exec:$PATH"
+export PATH="~/.dotfiles/git/exec:$PATH"
+
+source ~/.aliases
+if [ -f ~/.aliases.local ]; then
+    source ~/.aliases.local
+fi
+
+if [ -f ~/.bash_profile.local ]; then
+    source ~/.bash_profile.local
+fi
---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# git
+GIT_DIR=${BASEDIR}/git
+ln -s ${GIT_DIR}/gitconfig ~/.gitconfig
+ln -s ${GIT_DIR}/gitignore_global ~/.gitignore_global
+ln -s ${GIT_DIR}/plugins/git-completion.sh ~/.git-completion.sh
+ln -s ${GIT_DIR}/plugins/git-prompt.sh ~/.git-prompt.sh
+
+# vim
+VIM_DIR=${BASEDIR}/vim
+ln -s ${VIM_DIR}/vimrc ~/.vimrc
+ln -s ${VIM_DIR} ~/.vim
+git submodule init && git submodule update
+
+# tmux
+TMUX_DIR=${BASEDIR}/tmux
+if [ `uname` == 'Darwin' ]; then
+  brew install tmux
+  brew install cmake
+  cp  ${TMUX_DIR}/tmux.conf ~/.tmux.conf
+  echo "set -g default-terminal 'xterm-256color'" >> ~/.tmux.conf
+  echo "set -g default-command 'reattach-to-user-namespace -l ${SHELL}'" >> ~/.tmux.conf
+  echo "set -g pane-border-style fg='#a9b7c6'" >> ~/.tmux.conf
+elif [ `uname` == 'Linux' ]; then
+  ln -s ${TMUX_DIR}/tmux.conf ~/.tmux.conf
+fi
+
+# bash profile + remaining
+ln -s ${BASEDIR}/bash_profile ~/.bash_profile
+ln -s ${BASEDIR}/inputrc ~/.inputrc
+ln -s ${BASEDIR}/aliases ~/.aliases
+
+# make the tmux-cpu-mem-load plugin
+cd ${TMUX_DIR}/plugins/tmux-mem-cpu-load
+cmake .
+make
+make install
+cd ${BASEDIR}

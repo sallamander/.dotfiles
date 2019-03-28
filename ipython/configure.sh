427c2f33cc42f679ff899651066570c7d13cfdea
Sean Sall <sean@arterys.com>
2019-03-28 14:22:27 -0700

    Update IPython config, vimrc

--- ipython/configure.sh
+++ ipython/configure.sh
@@ -3,3 +3,12 @@
 ipython profile create
 rm ~/.ipython/profile_default/ipython_config.py
 ln -s ~/.dotfiles/ipython/ipython_config.py ~/.ipython/profile_default/ipython_config.py
+
+conda install -c conda-forge jupyter_contrib_nbextensions
+jupyter nbextensions_configurator enable --user
+
+mkdir -p $(jupyter --data-dir)/nbextensions
+cd -p $(jupyter --data-dir)/nbextensions
+
+git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+jupyter nbextension enable vim_binding/vim_binding
